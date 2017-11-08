e4754a71e2040aa6c4ae423d1278e4fc697f0482
Kankroc <github@belval.org>
2017-11-08 15:36:19 -0500

    Add download argument

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ A tool to convert a Wikipedia dump file into plain text
 
 ## How to use it
 
-Simply `python3 wikipedia2text.py enwiki-latest-pages-meta-current.xml.bz2 save.txt` and let it parse all of Wikipedia's content.
+Use `python3 wikipedia2text.py -h` to print usage.
+
+A typical usage would be like this `python3 wikipedia2text out.txt -dl fr,en`
+
+There are a few arguments you can use to change the "normal" behavior:
+
+- `-dl (--download_languages)` Allow you to specify one or more languages to download from wikipedia (imperatively separated by a comma).
+- `-i (--input_files)` Allow you to specify one or more files that were already downloaded (both options can be combined)
+- `-m (--mix_sentences)` Will shuffle the data to make it more uniform
 
 ## Where can I get the dump file?
 
-See https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-meta-current.xml.bz2
+See https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-meta-current.xml.bz2 or use `-dl en`
--- wikipedia2text.py
+++ wikipedia2text.py
@@ -1,29 +1,94 @@
 import sys
 import os
+import uuid
 import re
+import argparse
+import requests
 
 from bz2 import BZ2Decompressor
+
+def parse_arguments():
+    """
+        Parse the command line arguments of the program.
+    """
+
+    parser = argparse.ArgumentParser(description='Generate a corpus from Wikipedia.')
+
+    parser.add_argument(
+        "output_file",
+        type=str,
+        nargs="?",
+        help="The output file",
+        default="data.txt",
+    )
+
+    parser.add_argument(
+        "-dl",
+        "--download_languages",
+        type=str,
+        nargs="?",
+        help="The languages to download and use, with 2 char code (en,fr,de,es) separated by commas",
+        default="en"
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input_files",
+        nargs="+",
+        help="The input files to use to build the corpus.",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--mix_sentences",
+        action="store_true",
+        help="Define if the lines should be shuffled to allow for a more uniform distribution",
+        default=True
+    )
+
+    return parser.parse_args()
 
 def main(args):
     """
         wikipedia2text.py  [input_file] [output_file]
     """
 
-    input_file = args[1]
-    output_file = args[2]
+    args = parse_arguments()
+
+    input_files = args.input_files if args.input_files is not None else []
+    output_file = args.output_file
+    mix_sentences = args.mix_sentences
+    download_languages = args.download_languages.split(',') if args.download_languages is not None else []
+
+    print(download_languages)
 
     regex_remove_tag = re.compile(r"<.*?>")
     char_to_remove = ['{', '}', '[', ']', '<', '>', '=', '&', '"', '(', ')', '|', '/', '\\', '*', '\'', '#', ':']
     translation_table = str.maketrans(''.join(char_to_remove), ''.join([' '] * len(char_to_remove)))
 
+    if download_languages is not None and len(download_languages) > 0:
+        print('Starting download')
+        for lang in download_languages:
+            print('Downloading ' + lang + ' package')
+            r = requests.get('https://dumps.wikimedia.org/{}wiki/latest/{}wiki-latest-pages-meta-current.xml.bz2'.format(lang, lang), stream=True)
+            lang_filename = str(uuid.uuid4())
+            with open(lang_filename, 'wb') as f:
+                for i, chunk in enumerate(r.iter_content(chunk_size=1000 * 1024)):
+                    if i % 10 == 0:
+                        print(str(i) + ' MB downloaded')
+                    if chunk:
+                        f.write(chunk)
+                input_files.append(lang_filename)
+
     # We start by decompressing the file
     print('Starting decompression')
-    with open(output_file + '_decompressed', 'wb') as fout, open(input_file, 'rb') as fin:
-        decompressor = BZ2Decompressor()
-        for i, data in enumerate(iter(lambda : fin.read(1000 * 1024), b'')):
-            if i % 100 == 0:
-                print(str(i) + ' MB processed')
-            fout.write(decompressor.decompress(data))
+    for input_file in input_files:
+        with open(output_file + '_decompressed', 'wb') as fout, open(input_file, 'rb') as fin:
+            decompressor = BZ2Decompressor()
+            for i, data in enumerate(iter(lambda : fin.read(1000 * 1024), b'')):
+                if i % 100 == 0:
+                    print(str(i) + ' MB processed')
+                fout.write(decompressor.decompress(data))
 
     # We then remove whatever wouldn't go well in word2vec
     print('Starting line conversion')
